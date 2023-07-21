bcde229c43f073b53dfd09e90ad47cad4157e74a
GuiCezaF <contato.guicezafe@gmail.com>
2023-07-21 13:22:19 -0300

    feat: Initial Setup

---
+++ ToDoList.Domain/Entities/ToDo.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.Domain.Entities
+{
+    public class ToDo
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Finished { get; set; }
+    }
+}
