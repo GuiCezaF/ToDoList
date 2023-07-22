5bb1820e46f890e88b54a4b35a6811bf5f8a02ed
GuiCezaF <contato.guicezafe@gmail.com>
2023-07-22 14:56:07 -0300

    feat: add ToDo Map

---
+++ ToDoList.API/Migrations/20230722175450_MapToDoEntity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ToDoList.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class MapToDoEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Finished = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDo", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDo");
+        }
+    }
+}
---
+++ ToDoList.Infra.Data/Maps/ToDoMap.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoList.Domain.Entities;
+
+namespace ToDoList.Infra.Data.Maps
+{
+    public class ToDoMap : IEntityTypeConfiguration<ToDo>
+    {
+        public void Configure(EntityTypeBuilder<ToDo> builder)
+        {
+        }
+    }
+}
