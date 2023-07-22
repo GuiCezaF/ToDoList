7fdf1b59d5337c16140968a07ae120b4a0616c5e
GuiCezaF <contato.guicezafe@gmail.com>
2023-07-22 14:23:11 -0300

    feat: add db context

---
+++ ToDoList.Infra.Data/context/ToDoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Infra.Data.context
+{
+    public class ToDoDbContext : DbContext
+    {
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ToDoDbContext).Assembly);
+        }
+    }
+}
+}
