aea94f7448af6e5e48e3b09bf33ecb83024dcfa2
GuiCezaF <contato.guicezafe@gmail.com>
2023-07-24 13:34:08 -0300

    feat: add To Do DTO and validations

---
+++ ToDoList.Application/DTOS/ToDoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.Application.DTOS
+{
+    public class ToDoDTO
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool Finished { get; private set; } = false;
+    }
+}
