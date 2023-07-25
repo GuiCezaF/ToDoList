7b14bb0c39098b5f1b591f21d321e43be324cd2c
GuiCezaF <contato.guicezafe@gmail.com>
2023-07-25 12:08:37 -0300

    feat<ToDo>: add create to do service

---
+++ ToDoList.Application/Services/Interfaces/IToDoService.cs
@@ -0,0 +1,9 @@
+﻿using ToDoList.Application.DTOS;
+
+namespace ToDoList.Application.Services.Interfaces
+{
+    public interface IToDoService
+    {
+        Task<ResultService<ToDoDTO>> CreateAsync(ToDoDTO toDoDTO);
+    }
+}
---
+++ ToDoList.Application/Services/ToDoService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using ToDoList.Application.DTOS;
+using ToDoList.Application.DTOS.Validations;
+using ToDoList.Application.Services.Interfaces;
+using ToDoList.Domain.Entities;
+using ToDoList.Domain.Repositories.Interfaces;
+
+namespace ToDoList.Application.Services
+{
+    public class ToDoService : IToDoService
+    {
+        private readonly IToDoRepository _toDoRepository;
+        private readonly IMapper _mapper;
+
+        public ToDoService(IToDoRepository toDoRepository, IMapper mapper)
+        {
+            _toDoRepository = toDoRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ResultService<ToDoDTO>> CreateAsync(ToDoDTO toDoDTO)
+        {
+            if (toDoDTO == null)
+                return ResultService.Fail<ToDoDTO>("Objeto deve ser informado");
+
+            var result = new ToDoDTOValidator().Validate(toDoDTO);
+            if (!result.IsValid)
+                return ResultService.RequestError<ToDoDTO>("Problemas de validação", result);
+
+            var toDo = _mapper.Map<ToDo>(toDoDTO);
+            var data = await _toDoRepository.CreateAsync(toDo);
+
+            return ResultService.Ok<ToDoDTO>(_mapper.Map<ToDoDTO>(data));
+        }
+    }
+}
